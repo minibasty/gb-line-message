1c70a0ce6ac4e8fd9874b1f009981163102e2383
Nopparat <fanta.orange_@hotmail.com>
2020-04-08 17:33:25 +0700

    มาละแหละ

--- index.php
+++ index.php
@@ -19,14 +19,10 @@ if (sizeof($request_array['events']) > 0) {
 
     $reply_message = '';
     $reply_token = $event['replyToken'];
+    $text = $event['replyToken']['userId'];
     $data = [
-      'replyToken' => $reply_token,
-      'messages' => [
-        [
-          'type' => 'text',
-          'text' => 'อิหยังวะ'
-        ]
-      ]
+       'replyToken' => $reply_token,
+       'messages' => [['type' => 'text', 'text' => $text ]]
     ];
     $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
     $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
